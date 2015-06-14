a282a886fd4b52b7bbefd2c94e5e872d025c2f0a
huyhuynh408 <huyhuynh85@gmail.com>
2015-06-14 15:07:06 -0500

    added animate scroll to contacts anchor

--- to-the-top.js
+++ to-the-top.js
@@ -10,7 +10,13 @@ $(document).ready(function(){
 
   $('.scrollToTop').click(function(event){
     event.preventDefault();
-    $('html, body').animate({scrollTop : 0}, 300);
+    $('html, body').animate({scrollTop: 0}, 400);
+    return false;
+  });
+  $('.contact').click(function(event){
+    event.preventDefault();
+    console.log("hi");
+    $("html, body").animate({scrollTop: $(document).height()}, 400);
     return false;
   });
 });
